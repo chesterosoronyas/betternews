1eb4ab00b4d84dfa5799acfca4c239d4a283bda9
chesterosoronyas <conyasetia@gmail.com>
2025-02-24 19:00:31 +0300

    Lucia Auth

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,42 @@
 import { type ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
+import { lucia } from "./lucia";
+import type { Context } from "./context";
+import { authRouter } from "./routes/auth";
 
-const app = new Hono();
+const app = new Hono<Context>();
 
-app.get("/", (c) => {
+/*app.get("/", (c) => {
   //throw new Error("Unexpected");
   //throw new HTTPException(404, {message:"Post not found",cause :{form:true}});
   return c.text("Hello Hono!");
+});*/
+
+app.use("*",cors(), async (c, next) => {
+	const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
+	if (!sessionId) {
+		c.set("user", null);
+		c.set("session", null);
+		return next();
+	}
+
+	const { session, user } = await lucia.validateSession(sessionId);
+	if (session && session.fresh) {
+		c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
+	}
+	if (!session) {
+		c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
+	}
+	c.set("session", session);
+	c.set("user", user);
+	return next();
 });
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes=app.basePath("/api").route("/auth",authRouter);
 
 
 app.onError((err,c)=> {
@@ -28,3 +56,4 @@ app.onError((err,c)=> {
   },500)
 })
 export default app;
+export type ApiRoutes=typeof routes;
--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export  type SuccessResponse<T =void>={
     success:true;
     message:string;
@@ -16,3 +18,14 @@ export type ErrorResponse ={
     isFormError?:boolean
 }
 
+
+
+export const loginSchema = z.object({
+    username: z
+      .string()
+      .min(3)
+      .max(31)
+      .regex(/^[a-zA-Z0-9_]+$/),
+    password: z.string().min(3).max(255),
+  });
+  
