9d3d52528a47fc09a0e601509c315455b68cbe17
chesterosoronyas <conyasetia@gmail.com>
2025-02-24 16:10:49 +0300

    hono Error handling

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,30 @@
+import { type ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
 app.get("/", (c) => {
+  //throw new Error("Unexpected");
+  //throw new HTTPException(404, {message:"Post not found",cause :{form:true}});
   return c.text("Hello Hono!");
 });
 
+
+app.onError((err,c)=> {
+ 
+  if(err instanceof HTTPException){
+    const errResponse=err.res ?? c.json<ErrorResponse>({success:false,error:err.message, isFormError:err.cause && typeof err.cause === "object" && "form" in err.cause?
+      err.cause.form ===true :false,
+    },err.status)
+
+    return errResponse;
+  }
+
+
+  return c.json<ErrorResponse>({
+    success:false,
+    error:process.env.NODE_ENV === "production"? "Internal Server Error" : (err.stack?? err.message),
+  },500)
+})
 export default app;
